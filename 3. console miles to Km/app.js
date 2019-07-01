c8f333b4c46a634611a3ff752f05bf7918193cba
vstrog <39245801+vstrog@users.noreply.github.com>
2019-07-01 08:03:34 +0300

    Add files via upload

---
+++ 3. console miles to Km/app.js
@@ -0,0 +1,5 @@
+let numMiles = prompt("How many miles");
+let kiloNum = 1.60934;
+let kiloValue = (kiloNum * numMiles);
+let message = `${numMiles} miles is equal to ${kiloValue} Km` 
+console.log(message);
