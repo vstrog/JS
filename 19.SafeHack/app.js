252eea5666feb8ccab02c28ea0fc02b321f06d49
vstrog <39245801+vstrog@users.noreply.github.com>
2019-10-18 23:18:03 +0300

    Add files via upload

---
+++ 19.SafeHack/app.js
@@ -0,0 +1,58 @@
+const gameArea = document.querySelector(".game");
+const button = document.querySelector("button");
+const message = document.querySelector(".message");
+let score = 0;
+let gamePlay = false;
+button.addEventListener("click", function () {
+    if (!gamePlay) {
+        gamePlay = true;
+        score = 0;
+        gameArea.innerHTML = "";
+        maker(6);
+        message.innerHTML = "Guess the combo";
+        button.innerHTML = "Check Combo";
+    }
+    else {
+        score++;
+        message.innerHTML = "Guesses " + score;
+        const numbers = document.querySelectorAll(".numb");
+        let winCondition = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i].value == numbers[i].correct) {
+                numbers[i].style.backgroundColor = "green";
+                numbers[i].style.color = "white";
+                winCondition++;
+            }
+            else {
+                let color = (numbers[i].value < numbers[i].correct) ? "blue" : "red";
+                numbers[i].style.backgroundColor = color;
+                numbers[i].style.color = "black";
+            }
+            if (winCondition == numbers.length) {
+                gameEND();
+            }
+        }
+    }
+})
+
+function gameEND() {
+    message.innerHTML = "You solved the combo in  " + score + " guesses";
+    gamePlay = false;
+    button.innerHTML = "Restart Game";
+}
+
+function maker(num) {
+    for (let x = 0; x < num; x++) {
+        let el = document.createElement("input");
+        el.setAttribute("type", "number");
+        el.max = 9;
+        el.min = 0;
+        el.size = 1;
+        el.style.width = "50px";
+        el.classList.add("numb");
+        el.order = x;
+        el.correct = Math.floor(Math.random() * 10);
+        el.value = 0;
+        gameArea.appendChild(el);
+    }
+}
